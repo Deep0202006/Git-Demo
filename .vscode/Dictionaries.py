d66c0f1c54a34371f86723c0015348bdf7059ae8
Deep Patel <dcp6996@gmail.com>
2024-12-04 14:58:03 +0530

    second

---
+++ .vscode/Dictionaries.py
@@ -0,0 +1,10 @@
+my_dict={
+    'Name':'Deep',
+    'Age':18,
+    'City':'XYZ'
+}
+my_dict=dict(Name='Deep',Age='18',City='XYZ')
+
+print(my_dict['Name'])
+print(my_dict['Age'])
+print(my_dict['City'])
